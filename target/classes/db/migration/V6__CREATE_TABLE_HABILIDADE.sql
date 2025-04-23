20660a0bb4a31e3c8095f4f41f5bfaf11fcd7770
Enzo Teves <enzoalvesteves@gmail.com>
2025-04-23 17:44:22 -0300

    removido veersionamento

--- target/classes/db/migration/V6__CREATE_TABLE_HABILIDADE.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE habilidade
-(
-    id            INT AUTO_INCREMENT PRIMARY KEY,
-    candidato_id  INT,
-    descricao     VARCHAR(255) NOT NULL,
-    nivel         VARCHAR(50),
-    especialidade VARCHAR(100),
-    FOREIGN KEY (candidato_id) REFERENCES candidato (id)
-);
