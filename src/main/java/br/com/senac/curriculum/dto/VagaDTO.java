bea98d7fe1a2aae855f70f477ef2d4482856f6f2
Enzo Teves <enzoalvesteves@gmail.com>
2025-05-14 10:43:38 -0300

    Arrumando pagina home e criando vagas para salvar

--- src/main/java/br/com/senac/curriculum/dto/VagaDTO.java
+++ src/main/java/br/com/senac/curriculum/dto/VagaDTO.java
@@ -1,4 +1,31 @@
 package br.com.senac.curriculum.dto;
 
+import br.com.senac.curriculum.repository.vaga.VagaEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class VagaDTO {
+
+	private Long id;
+	private String titulo;
+	private String descricao;
+	private String empresa;
+	private String beneficios;
+	private double salario;
+
+	public VagaDTO() {
+	}
+
+	public VagaDTO(VagaEntity vagaEntity) {
+		this.id = vagaEntity.getId();
+		this.titulo = vagaEntity.getTitulo();
+		this.descricao = vagaEntity.getDescricao();
+		this.empresa = vagaEntity.getEmpresa();
+		this.beneficios = vagaEntity.getBeneficios();
+		this.salario = vagaEntity.getSalario();
+
+	}
+
 }
