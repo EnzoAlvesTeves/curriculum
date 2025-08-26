bb936b921d4e2c65ec2fda77ed9b030f0bac9665
Enzo Teves <enzoalvesteves@gmail.com>
2025-08-26 15:20:30 -0300

    Add new ms-curriculum

---
+++ ms-curriculum/src/main/java/br/com/senac/mscurriculum/MsCurriculumApplication.java
@@ -0,0 +1,13 @@
+package br.com.senac.mscurriculum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCurriculumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCurriculumApplication.class, args);
+	}
+
+}
---
+++ ms-curriculum/src/test/java/br/com/senac/mscurriculum/MsCurriculumApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senac.mscurriculum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCurriculumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
