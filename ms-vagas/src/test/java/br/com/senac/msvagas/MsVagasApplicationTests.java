ac7e2a333c86d5d4300a2a9d9f7d09c11e91a130
Enzo Teves <enzoalvesteves@gmail.com>
2025-08-27 13:31:48 -0300

    Add new project

---
+++ ms-vagas/src/test/java/br/com/senac/msvagas/MsVagasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senac.msvagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsVagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
