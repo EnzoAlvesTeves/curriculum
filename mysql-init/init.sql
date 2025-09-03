337ca299ed8589769ff63eff4e2daa6de3755545
Enzo Teves <enzoalvesteves@gmail.com>
2025-09-03 14:16:52 -0300

    Add docker-compose and mysql

---
+++ mysql-init/init.sql
@@ -0,0 +1,4 @@
+-- Criação dos bancos de dados
+CREATE USER IF NOT EXISTS 'user_curriculum'@'%' IDENTIFIED BY 'admin123';
+CREATE USER IF NOT EXISTS 'user_usuario'@'%' IDENTIFIED BY 'admin123';
+CREATE USER IF NOT EXISTS 'user_vagas'@'%' IDENTIFIED BY 'admin123';
