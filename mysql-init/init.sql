dc71c9ef3d06d0b1cd71cb2e109689092b21151f
Enzo Teves <enzoalvesteves@gmail.com>
2025-09-10 09:29:01 -0300

    Updated configurations in docker-compose.yaml and adjustments in MySQL init.sql

--- mysql-init/init.sql
+++ mysql-init/init.sql
@@ -1,4 +1,14 @@
 -- Criação dos bancos de dados
-CREATE USER IF NOT EXISTS 'user_curriculum'@'%' IDENTIFIED BY 'admin123';
-CREATE USER IF NOT EXISTS 'user_usuario'@'%' IDENTIFIED BY 'admin123';
-CREATE USER IF NOT EXISTS 'user_vagas'@'%' IDENTIFIED BY 'admin123';
+CREATE DATABASE IF NOT EXISTS ms_curriculum;
+CREATE DATABASE IF NOT EXISTS ms_usuario;
+CREATE DATABASE IF NOT EXISTS ms_vagas;
+
+-- Criar usuários específicos para cada banco
+CREATE USER IF NOT EXISTS 'user_curriculum'@'%' IDENTIFIED BY 'admin';
+CREATE USER IF NOT EXISTS 'user_usuario'@'%' IDENTIFIED BY 'admin';
+CREATE USER IF NOT EXISTS 'user_vagas'@'%' IDENTIFIED BY 'admin';
+
+-- Conceder privilégios aos usuários
+GRANT ALL PRIVILEGES ON ms_curriculum.* TO 'user_curriculum'@'%';
+GRANT ALL PRIVILEGES ON ms_usuario.* TO 'user_usuario'@'%';
+GRANT ALL PRIVILEGES ON ms_vagas.* TO 'user_vagas'@'%';
