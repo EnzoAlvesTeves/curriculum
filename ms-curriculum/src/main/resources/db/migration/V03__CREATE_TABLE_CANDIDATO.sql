29fc50b6134a5a17d16c889a45d292757e1edcb5
Enzo Teves <enzoalvesteves@gmail.com>
2025-09-18 16:04:46 -0300

    Rodando os banco de dados

--- ms-curriculum/src/main/resources/db/migration/V03__CREATE_TABLE_CANDIDATO.sql
+++ ms-curriculum/src/main/resources/db/migration/V03__CREATE_TABLE_CANDIDATO.sql
@@ -8,6 +8,5 @@ CREATE TABLE candidato (
     resumo_profissional VARCHAR(1000),
     endereco_id INT NOT NULL,
     usuario_id INT NOT NULL,
-    FOREIGN KEY (endereco_id) REFERENCES endereco (id),
-    FOREIGN KEY (usuario_id) REFERENCES usuario (id)
+    FOREIGN KEY (endereco_id) REFERENCES endereco (id)
 );
--- ms-vagas/src/main/resources/db/migration/V03__CREATE_TABLE_CANDIDATO_VAGA.sql
+++ ms-vagas/src/main/resources/db/migration/V03__CREATE_TABLE_CANDIDATO_VAGA.sql
@@ -3,6 +3,5 @@ CREATE TABLE candidato_vaga (
     candidato_id INT NOT NULL,
     vaga_id INT NOT NULL,
     data_inscricao TIMESTAMP NOT NULL,
-    FOREIGN KEY (candidato_id) REFERENCES candidato(id),
     FOREIGN KEY (vaga_id) REFERENCES vaga(id)
 );
