d9beae9682e3b076f9a632d890eb47199d781c35
Enzo Teves <enzoalvesteves@gmail.com>
2025-10-09 13:51:59 -0300

    Feat: add new bff-curriculum

---
+++ bff-curricukum/src/main/java/br/com/senac/bffcurricukum/BffCurricukumApplication.java
@@ -0,0 +1,13 @@
+package br.com.senac.bffcurricukum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffCurricukumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffCurricukumApplication.class, args);
+	}
+
+}
